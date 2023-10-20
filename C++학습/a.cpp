4e483f1925a83077165bc0070a33ab5d4577bb01
Limjuhan <72773649+Limjuhan@users.noreply.github.com>
2023-10-20 17:28:54 +0900

    Add files via upload

--- C++학습/a.cpp
+++ C++학습/a.cpp
@@ -1846,12 +1846,61 @@ ld:long long
 //	return 0;
 //}
 
+//중첩for문
 
+//#include<bits/stdc++.h> 
+//using namespace std;
+//
+//int n = 5;
+//int k = 3;
+//int a[5] = {1,2,3,4,5};
+//int main () {
+//	for(int i=0; i<n; i++) {
+//		for(int j=i+1; j<n; j++) {
+//			for(int k=j+1; k<n; k++) {
+//				cout << i << " " << j << " " << k << '\n';
+//			}
+//		}
+//	}
+//	return 0;
+//}
 
+//#include<bits/stdc++.h>
+//using namespace std;
+//int n=5, k=3, a[5]={1,2,3,4,5};
+//
+//int main() {
+//	for(int i=0; i<n; i++) {
+//		for(int j=0; j<i;j++) {
+//			for(int k=0; k<j; k++) {
+//				cout << i << " " << j<< " " << k << '\n';
+//			}
+//		}
+//	}
+//	return 0;
+//}
 
+//r이 2개를 뽑는것이라면..
 
-
-
+//#include <cstdio> 
+//#include <algorithm>
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int n=5;
+//int k=2;
+//int a[5] = {1,2,3,4,5};
+//
+//int main() {
+//	for(int i=0; i<n; i++) {
+//		for(int j=i+1; j<n; j++) {
+//			cout << i << " " << j << '\n';
+//		}
+//	}
+//	return 0;
+//}
 
 
 
